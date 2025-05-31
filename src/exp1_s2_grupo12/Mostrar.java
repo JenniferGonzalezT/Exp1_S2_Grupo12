19bd51b12cd9eed4f72ea900cf853e3836af375f
Jennifer Gonzalez <je.gonzalezt@duocuc.cl>
2025-05-31 17:44:26 -0400

    Agrego interfaz Mostrar

---
+++ src/exp1_s2_grupo12/Mostrar.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package exp1_s2_grupo12;
+
+/**
+ * Interfaz implementada en la clase Cliente
+ * @author jennifer y guiselle
+ */
+
+public interface Mostrar {
+    public void mostrarInformacion();
+}
